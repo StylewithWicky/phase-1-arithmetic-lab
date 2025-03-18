b27ccdb20335545121bd6b263a1e6d1c52bf139f
Yazz <itsjustmaitai@gmail.com>
2025-03-18 12:26:04 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write your code he
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1; 
+
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4; // 28 % 6 = 4
+
+
+const max = Math.max(10, 15, 20, 5);
+
+module.exports = { multiply, random, mod, max };
